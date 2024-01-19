87a9f9b195e28ac70198a8e2c78265f6b958b6ee
Paloma <paloma.sanchezc@icloud.com>
2024-01-19 16:49:19 +0100

    zoom in and out

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
+const  { innerHeight } = window;
+// const innerHeight = window.innerHeight;
 
-gsap.to(".c", {
-    scrollTrigger:{
-        trigger :".c",
-        start:"top center",
-        markers : true,
-        toggleActions : "restart none none none"
+gsap.from("#zoomOut h2",{
+    scale:50,
+    stager : 0.25,
+    duration: 3,
+    scrollTrigger :{
+        trigger: "#zoomOut",
+        pin :true,
+        end:`+=${innerHeight * 1.3}`,
+        scrub : 3
+    }
+})
 
-    },
-    x:400,
-    rotation:360,
-    duration:3
-});
+gsap.to("#zoomIn h2",{
+    scale:100,
+    stager : 0.25,
+    duration: 3,
+    scrollTrigger :{
+        trigger: "#zoomIn",
+        pin :true,
+        end:`+=${innerHeight * 1.3}`,
+        scrub : 3
+    }
+})
